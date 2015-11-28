8da43ad60b32c9e27f758ff57523b46b9ac1d704
Akhil Thampy <akhilthampy@yahoo.com>
2015-11-28 17:57:42 -0600

    Main: Adding argc check and VM creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
+#include<SDL.h>
 #include<chip8.h>
 #include<iostream>
 
-int main(int argc, char** argv){
+int main(int argc, char *argv[]){
 	if(argc<2){
 		std::cout <<"USAGE chip8 game.rom"<<std::endl;	
 		return 0;
 	}
-
-
+	chip8 CHIP8("asdada");
 }
