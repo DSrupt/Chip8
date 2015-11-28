733e02c681e53c7caf63f563fb847521258108e8
Akhil Thampy <akhilthampy@yahoo.com>
2015-11-28 13:45:08 -0600

    Started working on init and reset. Fixed include dir issue in CMakeLists.txt.

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,1 +1,33 @@
+#include<chip8.h>
+/*
+	TODO
+	file I/O
+	SDL setup/destroy
+	SDL keyboard input
+	SDL draw
+	decoder
+*/
+chip8::chip8(){
+	init();	
+}
+chip8::init(){	
+	reset();
+	PC = 0x200;
+	Delay_timer = 0;
+	Sound_timer = 0;
+	Index = 0;
+}
+chip8::reset(){
+	//TODO look into std::fill
+	memset(Reg, 0, sizeof(Reg)); 
+	memset(Memory, 0, sizeof(Memory));
+	memset(display, 0, sizeof(display));
+	_stack = std::stacki<unsigned char>();
+	//load charset
+	for(int i=0;i<80;i++){
+		Memory[i] = charset[i];
+	}
+}
+chip8::cycle(){
 
+}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include<chip8.h>
+#include<iostream>
+
+int main(int argc, char** argv){
+	if(argc<2){
+		std::cout <<"USAGE chip8 game.rom"<<std::endl;	
+		return 0;
+	}
+
+
+}
